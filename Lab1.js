573bc82bfffea8433bd03eb06bf769c920dbcf35
AnichkaT <anntverdokhlib@gmail.com>
2024-10-03 19:06:47 +0300

    Add to report lab 1 and 2

---
+++ Lab1.js
@@ -0,0 +1,127 @@
+function firstButton() {
+    var textDiv = document.getElementById('text');
+
+    textDiv.textContent = '';
+
+    textDiv.innerHTML = ' <p>Тема: Бібліотека української класики</p>';
+    textDiv.innerHTML += '<p>Мета: Полегшення знаходження інформація про авторів та книги, читання книг</p>';
+    textDiv.innerHTML += '<p>Опис предметної області: Застосунок дозволяє переглядати'+
+    'всю доступну інформацію про книгу (її автора, рік видання та ін. ), читати ці'+
+    'книги, перегляд всієї доступної інформації про автора</p>';
+    textDiv.innerHTML += 'Сценарій:'+
+            '<ul> <li>можливість авторизації</li>'+
+            '<li>можливість додавання книги</li>'+
+            '<li>можливість перегляду інформації про книгу</li>'+
+            '<li>можливість перегляду інформації про автора</li>'+
+            '</ul>';
+    
+    
+    
+}
+
+function secondButton() {
+    var textDiv = document.getElementById('text');
+
+    textDiv.textContent = '';
+
+    textDiv.innerHTML = ' <p>Тема: структура HTML-документа. '+
+    'Вибір предметної  галузі. Робота з посиланнями, таблицями,'+
+    'зображеннями, списками в HTML-документі.</p>';
+    textDiv.innerHTML += '<p>Мета: придбати практичні навички роботи  з HTML-документом, таблицями, '+
+    'зображеннями, посиланнями, списками, формами, створити шаблон звітного HTML-документом '+
+    'для відображення результатів роботи всіх лабораторних робіт</p>';
+
+    const links = [
+        { url: 'https://github.com/AnichkaT/IC-34_Course_Tverdokhlib.git', text: 'Посилання на репозиторій веб застосунку  ' },
+        { url: 'https://anichkat.github.io/IC-34_Course_Tverdokhlib/', text: 'Посилання на живу сторінку веб застосунку  ' },
+        { url: 'https://github.com/AnichkaT/zvit.git', text: 'Посилання на репозиторій звіту  ' },
+        { url: 'https://anichkat.github.io/zvit/', text: 'Посилання на живу сторінку звіту  ' },
+        { url: 'https://github.com/AnichkaT/Independ.git ', text: 'Посилання на репозиторій самостійної роботи  ' },
+        { url: ' https://anichkat.github.io/Independ/', text: 'Посилання на живу сторінку самостійної роботи  ' }
+      ];
+
+
+
+    links.forEach(link => {
+        
+        const newLink = document.createElement('a');
+        newLink.href = link.url; 
+        newLink.textContent = link.url; 
+        textDiv.innerHTML +=link.text
+        textDiv.appendChild(newLink);
+
+        textDiv.appendChild(document.createElement('br'));
+    });
+}
+
+function thirdButton() {
+    var textDiv = document.getElementById('text');
+
+    textDiv.textContent = '';
+
+    textDiv.innerHTML = ' <p> Код таблиць виглядає наступним чином </p>';
+    const imageUrl = 'Screenshot 2024-10-03 154825.png';
+    
+    const newImage = document.createElement('img');
+    newImage.src = imageUrl; 
+    newImage.alt = 'Зображення'; 
+    textDiv.appendChild(newImage);
+    
+}
+
+function fourthButton() {
+    var textDiv = document.getElementById('text');
+
+    textDiv.textContent = '';
+
+    textDiv.innerHTML = ' <p> Код форми виглядає наступним чином </p>';
+    const imageUrl = 'Screenshot 2024-10-03 155108.png';
+    
+    const newImage = document.createElement('img');
+    newImage.src = imageUrl; 
+    newImage.alt = 'Зображення'; 
+    textDiv.appendChild(newImage);
+    
+}
+
+function fifthButton() {
+    var textDiv = document.getElementById('text');
+
+    textDiv.textContent = '';
+
+    textDiv.innerHTML = ' <p> Код фото виглядає наступним чином </p>';
+    const imageUrl = 'Screenshot 2024-10-03 155521.png';
+    
+    const newImage = document.createElement('img');
+    newImage.src = imageUrl; 
+    newImage.alt = 'Зображення'; 
+    textDiv.appendChild(newImage);
+    
+}
+
+
+function conclusionButton() {
+    var textDiv = document.getElementById('text');
+
+    textDiv.textContent = '';
+    textDiv.innerHTML ='<p align="center">Висновок</p>';
+    textDiv.innerHTML += ' <p> Я придбала практичні навички роботи  з HTML-документом, таблицями, зображеннями, посиланнями, списками та формами.'+
+    ' Навчилася створювати шаблон звітного HTML-документа для відображення результатів робіт всіх лабораторних. </p>';
+    textDiv.innerHTML += 'Контрольні питання:'+
+            '<ul> <li>1. Така структура  html, head, body, footer, main, header</li>'+
+            '<li>2. Теги – це спеціальні елементи розмітки, що визначають, як даний документ HTML має інтерпретуватися програмою перегляду інформації – браузером.</li>'+
+            '<li>3. Використовуються теги table, tr, th, td</li>'+
+            '<li>4. Використовуються тег img</li>'+
+            '<li>5. Використовуються теги ul,dl,ol,li</li>'+
+            '<li>6. Для запису таблиці використовують такі теги table, tr, th, td, caption</li>'+
+            '</ul>';
+    
+    
+}
+
+
+
+
+
+
+
---
+++ Lab2.js
@@ -0,0 +1,119 @@
+function firstButton() {
+    var textDiv = document.getElementById('text');
+
+    textDiv.textContent = '';
+
+    textDiv.innerHTML = ' <p>Тема: Класифікація таблиць стилів. Селектори. '+
+    'Індентифікатори. Стильове оформлення текстових документів в HTML- документах</p>';
+
+    textDiv.innerHTML += '<p>Мета: придбати практичні навички роботи  з селекторами, ідентифікаторами, '+
+    'списками,  різноманітними властивостями кольору і фону,  зовнішними та  '+
+    'внутрішними  відступами,  плаваючими елементами, оформленням текстових елементів</p>';
+
+    const links = [
+        { url: 'https://github.com/AnichkaT/IC-34_Course_Tverdokhlib.git', text: 'Посилання на репозиторій веб застосунку  ' },
+        { url: 'https://anichkat.github.io/IC-34_Course_Tverdokhlib/', text: 'Посилання на живу сторінку веб застосунку  ' },
+        { url: 'https://github.com/AnichkaT/zvit.git', text: 'Посилання на репозиторій звіту  ' },
+        { url: 'https://anichkat.github.io/zvit/', text: 'Посилання на живу сторінку звіту  ' },
+        { url: 'https://github.com/AnichkaT/Independ.git ', text: 'Посилання на репозиторій самостійної роботи  ' },
+        { url: ' https://anichkat.github.io/Independ/', text: 'Посилання на живу сторінку самостійної роботи  ' }
+      ];
+
+
+
+    links.forEach(link => {
+        
+        const newLink = document.createElement('a');
+        newLink.href = link.url; 
+        newLink.textContent = link.url; 
+        textDiv.innerHTML +=link.text
+        textDiv.appendChild(newLink);
+
+        textDiv.appendChild(document.createElement('br'));
+    });
+}
+
+function secondButton() {
+    var textDiv = document.getElementById('text');
+
+    textDiv.textContent = '';
+    textDiv.innerHTML += 'Є наступні способи підключення стилів до документу:'+
+    '<ul> <li> Вбудовані стилі (підключення стилів за допомогою атрибуту style.)</li>'+
+    '<li>  Внутрішні стилі (написання внутрішних стилів у тезі style)</li>'+
+    '<li> Зовнішні стилі (написання зовнішних стилів в окремому файлі з розширенням .css, підключається до сторінки html через тег <link>)</li>'+
+    '<li> Імпорти (Іпорт інших CSS файлів в інший CSS файл)</li>'+
+    '</ul>';
+    ;
+    
+}
+
+function thirdButton() {
+    var textDiv = document.getElementById('text');
+
+    textDiv.textContent = '';
+    textDiv.innerHTML = ' <p>Селектор тегу дозволює стилізувати весь вміст даного тегу.</p>';
+    textDiv.innerHTML += ' <p>Наприклад можна визначити такий селектор тегу як buttom у ньому можна прописати, якого кольору, розміру чи форми будуть всі кнопки на сторінці </p>';
+    textDiv.innerHTML += ' <pre> button{'+
+    'background-color: #04AA6D;'+
+    'color: white;'+
+    'margin: 10px ;'+
+    'text-align: center;'+
+    '} </pre>';
+
+
+    
+}
+
+
+function fourthButton() {
+    var textDiv = document.getElementById('text');
+
+    textDiv.textContent = '';
+
+    textDiv.innerHTML = ' <p>Селектор класу дозволює стилізувати елементи  даного класу.</p>';
+    textDiv.innerHTML += ' <p>Наприклад можна визначити такий селектор класу як labButtoms у ньому можна прописати, якого кольору, розміру чи форми будуть всі кнопки, в яких зазначено даний клас </p>';
+
+    textDiv.innerHTML += ' <pre> .labButtoms {'+
+    'background-color: #04AA6D;'+
+    'color: white;'+
+    'margin: 10px ;'+
+    'text-align: center;'+
+    '} </pre>';
+
+    
+}
+
+function fifthButton() {
+    var textDiv = document.getElementById('text');
+
+    textDiv.textContent = '';
+    textDiv.innerHTML = ' <p>Селектор індетифікотора дозволює стилізувати елемент з чим індитифікатором.</p>';
+    textDiv.innerHTML += ' <p>Наприклад можна визначити такий селектор індетифікатора як #firstParagraf у ньому можна прописати, якого кольору, розміру чи розташування буде текст данного параграфа з індетифікатором firstParagraf</p>';
+    textDiv.innerHTML += ' <pre> #firstParagraf{'+
+    'color: #04AA6D;'+
+    'text-align: center;'+
+    '} </pre>';
+    
+}
+
+
+function conclusionButton() {
+    var textDiv = document.getElementById('text');
+
+    textDiv.textContent = '';
+    textDiv.innerHTML ='<p align="center">Висновок</p>';
+    textDiv.innerHTML += ' <p> Я придбала практичні навички роботи  з селекторами, ідентифікаторами,'+
+    'списками,  різноманітними властивостями кольору і фону,  зовнішними та'+
+    ' внутрішними  відступами,  плаваючими елементами, оформленням текстових елементів</p>';
+    textDiv.innerHTML += 'Контрольні питання:'+
+            '<ul> <li>1. Каскадні таблиці стилів описують правила стилізації елементів за допомогою властивостей та допустимих для них значень. </li>'+
+            '<li>2. Існують такі методи визначення стилів - вбудовані стилі, внутрішні стилі, зовнішні стилі, імпорти </li>'+
+            '<li>3. Вони виконуються ось так (.labButtons {} - селектор класу, button {} - cелектор тегу )</li>'+
+            '<li>4. Ідентифікатор  в HTML використовується для ідентифікації окремого елемента на веб-сторінці. Він дозволяє надавати специфічні стилі та функціональність певному елементу.</li>'+
+            '<li>5. Наприклад: color, font-family, text-align, line-height </li>'+
+            '</ul>';
+    
+    
+}
+
+
